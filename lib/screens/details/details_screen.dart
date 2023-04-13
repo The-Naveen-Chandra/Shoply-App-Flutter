a3c9dc2d6ffa513a3fdc44d2f53503f29335fe2f
Naveen Chandra <the.naveenchandra@gmail.com>
2023-04-13 15:08:29 +0530

    Added: Appbar

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoply_app/constants.dart';
 import 'package:shoply_app/models/product.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -11,17 +12,40 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       // each product have a unique color
       backgroundColor: product.color,
-      appBar: AppBar(
-        backgroundColor: product.color,
-        elevation: 0,
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
-            size: 30,
-          ),
-          onPressed: () => Navigator.pop(context),
+      appBar: buildAppBar(context),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: product.color,
+      elevation: 0,
+      leading: IconButton(
+        icon: const Icon(
+          Icons.arrow_back,
+          size: 30,
         ),
+        onPressed: () => Navigator.pop(context),
       ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.search_outlined,
+            size: 30,
+            color: Colors.white,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.shopping_cart_outlined,
+            size: 30,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(width: kDefaultPadding / 2,)
+      ],
     );
   }
 }
